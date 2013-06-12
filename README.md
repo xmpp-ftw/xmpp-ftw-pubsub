c94144caf7a52f3e3ce807d0668d2d80cc72b692
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-06-12 21:03:44 +0100

    Initial pubsub commit

---
+++ README.md
@@ -0,0 +1,26 @@
+xmpp-ftw-pubsub
+===============
+
+A Publish-Subscribe/PubSub (XEP-0060) plugin for XMPP-FTW.
+
+https://github.com/lloydwatkin/xmpp-ftw.git
+
+# More....
+
+See the XMPP-FTW demo repository - https://github.com/lloydwatkin/xmpp-ftw-demo
+
+# Build status
+
+[![Build Status](https://secure.travis-ci.org/lloydwatkin/xmpp-ftw-pubsub.png)](http://travis-ci.org/lloydwatkin/xmpp-ftw-pubsub)
+
+# Install
+
+```
+npm i --save xmpp-ftw-pubsub
+```
+
+# Test
+
+```
+npm test
+```
---
+++ test/helper.js
@@ -0,0 +1,18 @@
+var ltx = require('ltx')
+  , fs = require('fs')
+  , Event = require('events').EventEmitter
+
+exports.getStanza = function(file) {
+    var stanza = fs.readFileSync(__dirname + '/resources/' + file)
+    var stanzaStr = stanza.toString()
+        .replace(/[\n\r]/g, '')
+        .replace(/>\s{1,}/g, '>')
+    return ltx.parse(stanzaStr)
+}
+
+var Eventer = function() {}
+Eventer.prototype = new Event()
+Eventer.prototype.send = function(stanza) {
+    this.emit('stanza', stanza.root())
+}
+exports.Eventer = Eventer
