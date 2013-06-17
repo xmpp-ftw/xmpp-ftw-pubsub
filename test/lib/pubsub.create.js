9301ddfae681628b6c70e03ba5679ffe6678213a
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-06-17 21:55:04 +0100

    Create a set of test files for pubsub

---
+++ test/lib/pubsub.create.js
@@ -0,0 +1,27 @@
+var should  = require('should')
+  , PubSub  = require('../../lib/pubsub')
+  , ltx     = require('ltx')
+  , helper  = require('../helper')
+
+describe('Publish-Subscribe', function() {
+
+    var pubsub, socket, xmpp, manager
+
+    before(function() {
+        socket = new helper.Eventer()
+        xmpp = new helper.Eventer()
+        manager = {
+            socket: socket,
+            client: xmpp,
+            trackId: function(id, callback) {
+                this.callback = callback
+            },
+            makeCallback: function(error, data) {
+                this.callback(error, data)
+            }
+        }
+        pubsub = new PubSub()
+        pubsub.init(manager)
+    })
+
+})
