78bc903d1aa1295b8679de46c2e5e5efb828dd00
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-06-22 19:36:14 +0100

    Publish: Add outline of tests for publishing items

--- test/lib/pubsub.items.js
+++ test/lib/pubsub.items.js
@@ -24,4 +24,63 @@ describe('Publish-Subscribe', function() {
         pubsub.init(manager)
     })
 
+    describe('Publishing items', function() {
+
+        it('Errors if \'to\' key missing', function(done) {
+            done('Not implemented yet')
+        })
+ 
+        it('Errors if \'node\' key missing', function(done) {
+            done('Not implemented yet')
+        })
+
+        it('Errors if no message content', function(done) {
+            done('Not implemented yet')
+        })
+ 
+        it('Errors if empty message content', function(done) {
+
+        })
+
+        it('Returns error if item content can be built', function(done) {
+            done('Not implemented yet')
+        })
+
+        it('Errors if publish options can\'t be parsed', function(done) {
+            done('Not implemented yet')
+        })
+
+        it('Sends expected stanza', function(done) {
+           done('Not implemented yet')
+        })
+
+        it('Sends expected stanza with set ID', function(done) {
+           done('Not implemented yet') 
+        })
+
+        it('Sends expected stanza with publish options', function(done) {
+           done('Not implemented yet')
+        })
+
+        it('Handles error stanza response', function(done) {
+            done('Not implemented yet')
+        })
+
+        it('Returns post ID on success', function(done) {
+
+        })
+
+    })
+
+    describe('Retrieving node items', function() {
+
+    })
+
+    describe('Deleting node items', function() {
+
+    })
+
+    describe('Purging a node', function() {
+
+    })
 })
