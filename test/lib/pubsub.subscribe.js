2872813a599cdff07122fefc6d030686fae39dc0
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-06-22 13:27:15 +0100

    Unsubscribe: All tests added. Fixes #6.

--- test/lib/pubsub.subscribe.js
+++ test/lib/pubsub.subscribe.js
@@ -165,4 +165,120 @@ describe('Publish-Subscribe', function() {
 
     })
 
+    describe('Unsubscribe', function() {
+
+        it('Errors if no \'to\' key provided', function(done) {
+            var request = {}
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            var callback = function(error, success) {
+                should.not.exist(success)
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing 'to' key")
+                error.request.should.eql(request)
+                xmpp.removeAllListeners('stanza')
+                done()
+            }
+            socket.emit('xmpp.pubsub.unsubscribe', request, callback)
+        })
+
+        it('Errors if no \'node\' key provided', function(done) {
+            var request = { to: 'pubsub.shakespeare.lit' }
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            var callback = function(error, success) {
+                should.not.exist(success)
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing 'node' key")
+                error.request.should.eql(request)
+                xmpp.removeAllListeners('stanza')
+                done()
+            }
+            socket.emit('xmpp.pubsub.unsubscribe', request, callback)
+        })
+
+        it('Sends expected stanza', function(done) {
+            var request = {
+                to: 'pubsub.shakespeare.lit',
+                node: 'twelfth night'
+            }
+            xmpp.once('stanza', function(stanza) {
+                stanza.is('iq').should.be.true
+                stanza.attrs.to.should.equal(request.to)
+                stanza.attrs.type.should.equal('set')
+                stanza.attrs.id.should.exist
+                stanza.getChild('pubsub', pubsub.NS_PUBSUB).should.exist
+                var pubsubElement = stanza.getChild('pubsub')
+                pubsubElement.getChild('unsubscribe').should.exist
+                pubsubElement.getChild('unsubscribe').attrs.node
+                    .should.equal(request.node)
+                done()
+            })
+            socket.emit('xmpp.pubsub.unsubscribe', request)
+        })
+
+        it('Sends expected stanza when \'subscription id\'', function(done) {
+            var request = {
+                to: 'pubsub.shakespeare.lit',
+                node: 'twelfth night',
+                id: '123456'
+            }
+            xmpp.once('stanza', function(stanza) {
+                stanza.getChild('pubsub')
+                    .getChild('unsubscribe')
+                    .attrs.subid.should.equal('123456')
+                done()
+            })
+            socket.emit('xmpp.pubsub.unsubscribe', request)
+        })
+
+        it('Handles an error stanza response', function(done) {
+            xmpp.once('stanza', function(stanza) {
+                manager.makeCallback(helper.getStanza('iq-error'))
+            })
+            var callback = function(error, success) {
+                should.not.exist(success)
+                error.should.eql({
+                    type: 'cancel',
+                    condition: 'error-condition'
+                })
+                done()
+            }
+            var request = {
+                to: 'pubsub.shakespeare.lit',
+                node: 'twelfth night'
+            }
+            socket.emit(
+                'xmpp.pubsub.unsubscribe',
+                request,
+                callback
+            )
+        })
+
+        it('Handles success response', function(done) {
+            xmpp.once('stanza', function(stanza) {
+                manager.makeCallback(helper.getStanza('iq-result'))
+            })
+            var callback = function(error, success) {
+                should.not.exist(error)
+                success.should.be.true
+                done()
+            }
+            var request = {
+                to: 'pubsub.shakespeare.lit',
+                node: 'twelfth night'
+            }
+            socket.emit(
+                'xmpp.pubsub.unsubscribe',
+                request,
+                callback
+            )
+        })
+
+    })
+
 })
